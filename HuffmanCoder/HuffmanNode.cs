5aac4d5789a0361ca9bfaa464350cfcaf725fbfe
uniqsoftco <50379296+uniqsoftco@users.noreply.github.com>
2019-05-09 04:53:41 +0430

    Initial commit

---
+++ HuffmanCoder/HuffmanNode.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HuffmanCoder
+{
+    public class HuffmanNode : IComparable<HuffmanNode>
+    {
+        public char chr;
+        public int frequency;
+        public string code;
+        public HuffmanNode parent;
+        public HuffmanNode left;
+        public HuffmanNode right;
+        public bool isLeaf;
+
+        public HuffmanNode(char val)
+        {
+            chr = val;
+            frequency = 1;
+            code = "";
+            parent = left = right = null;
+            isLeaf = true;
+        }
+
+        public HuffmanNode(HuffmanNode node1, HuffmanNode node2)
+        {
+            code = "";
+            isLeaf = false;
+            parent = null;
+
+            if (node1.frequency >= node2.frequency)
+            {
+                right = node1;
+                left = node2;
+            }
+            else if (node2.frequency > node1.frequency)
+            {
+                right = node2;
+                left = node1;
+            }
+            else
+            {
+                return;
+            }
+
+            right.code += "1";
+            left.code += "0";
+            node1.parent = node2.parent = this;
+            this.frequency = node1.frequency + node2.frequency;
+        }
+
+        public void CollectCodes(string prevCodes, Dictionary<char, string> codingDictionary)
+        {
+            if (isLeaf)
+            {
+                codingDictionary.Add(this.chr, prevCodes + this.code);
+            }
+            else
+            {
+                left.CollectCodes(prevCodes + this.code, codingDictionary);
+                right.CollectCodes(prevCodes + this.code, codingDictionary);
+            }
+        }
+
+        public int CompareTo(HuffmanNode otherNode)
+        {
+            return this.frequency.CompareTo(otherNode.frequency);
+        }
+    }
+}
---
+++ HuffmanCoder/Program.cs
@@ -0,0 +1,75 @@
+﻿//By Saeed Shirvani
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace HuffmanCoder
+{
+    class Program
+    {
+        private static List<HuffmanNode> NodeList = new List<HuffmanNode>();
+        private static Dictionary<char, string> CodingDictonary = new Dictionary<char, string>();
+        static void Main(string[] args)
+        {
+            Console.Write("Please type your text and press Enter: ");
+            string text = Console.ReadLine();
+
+            if (text == string.Empty)
+                return;
+
+            //Add first char to list
+            NodeList.Add(new HuffmanNode(text[0]));
+
+            //Add remaining chars to list
+            for (int i = 1; i <= text.Length - 1; i++)
+            {
+                HuffmanNode existingNode = NodeList.Find(n => n.chr == text[i]);
+                if (existingNode == null)
+                {
+                    NodeList.Add(new HuffmanNode(text[i]));
+                }
+                else
+                {
+                    existingNode.frequency++;
+                }
+            }
+
+            //sort the list
+            NodeList.Sort();
+
+            while (NodeList.Count > 1)
+            {
+                NodeList.Add(new HuffmanNode(NodeList[0], NodeList[1]));
+                NodeList.RemoveRange(0, 2);
+                NodeList.Sort();
+            }
+
+            //collect huffman codes
+            NodeList[0].CollectCodes(string.Empty, CodingDictonary);
+
+            Console.WriteLine("================================================");
+            Console.WriteLine(text);
+            Console.WriteLine("================================================");
+
+            foreach (KeyValuePair<char, string> keyValuePair in CodingDictonary)
+            {
+                Console.WriteLine($"{keyValuePair.Key}: {keyValuePair.Value}");
+            }
+            Console.WriteLine("================================================");
+
+            string codedText = "";
+            for (int i = 0; i <= text.Length - 1; i++)
+            {
+                codedText += CodingDictonary[text[i]];
+            }
+            Console.WriteLine("Huffman coded text: " + codedText);
+            Console.WriteLine("================================================");
+
+            Console.ReadLine();
+        }
+    }
+}
